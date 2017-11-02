993d7568a75361576f7c566747951fe83c99d074
Marcos Kruger <marcos.kruger@jv01.local>
2017-11-02 13:06:04 -0200

    Implementação do carousel boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GalleryProductComponent } from './gallery-product/gallery-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryProductComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/gallery-product/gallery-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-product',
+  templateUrl: './gallery-product.component.html',
+  styleUrls: ['./gallery-product.component.css']
+})
+export class GalleryProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
