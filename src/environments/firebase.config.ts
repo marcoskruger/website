81b0a0475c79786e29b24e5af6008bb89a096f58
Marcos Krüge <kr.marcos@gmail.com>
2017-11-06 00:16:42 -0200

    correção de navegação.

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,8 +1,3 @@
 export const FirebaseConfig = {
-    apiKey: 'AIzaSyDE225lq_uQltTqkMQxAFUjf78ozF9Fa3M',
-    authDomain: 'produtos-5abd3.firebaseapp.com',
-    databaseURL: 'https://produtos-5abd3.firebaseio.com',
-    projectId: 'produtos-5abd3',
-    storageBucket: 'produtos-5abd3.appspot.com',
-    messagingSenderId: '522817427066'
+    
 };
